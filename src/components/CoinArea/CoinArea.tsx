2e073602e12fe7afe44a42d752cf71d9bbc992f0
sherlyfebrianti96 <sherlyfebrianti96@yahoo.co.id>
2021-04-10 05:15:16 +0200

    Change the Coin Area Design

--- src/components/CoinArea/CoinArea.tsx
+++ src/components/CoinArea/CoinArea.tsx
@@ -1,26 +1,29 @@
 import React from "react";
 import './CoinArea.css';
+import {Card, Container} from "react-bootstrap";
 
 interface CoinAreaProps {
-    name: string;
-    title: string;
+  name: string;
+  title: string;
 }
 
 class CoinArea extends React.Component<CoinAreaProps> {
-    render() {
-        const areaId = `coin-area-${this.props.name}`;
+  render() {
+    const areaId = `coin-area-${this.props.name}`;
 
-        return (
-            <div className="Coin-Area" id={areaId}>
-                <h2>{this.props.title}</h2>
-                <div className="Coin-Box">
-                </div>
-                <div className="CoinInformation">
-                  Current box value = 0
-                </div>
-            </div>
-        );
-    };
+    return (
+      <Card className="Coin-Area" id={areaId}>
+        <Card.Header>
+          <h2>{this.props.title}</h2>
+        </Card.Header>
+        <Card.Body className="Coin-Box">
+        </Card.Body>
+        <Card.Footer className="CoinInformation">
+          Current box value = 0
+        </Card.Footer>
+      </Card>
+    );
+  };
 }
 
 export default CoinArea;
