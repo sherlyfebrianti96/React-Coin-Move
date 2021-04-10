4d0025bc870c12b7a18bdd876dc70aaa8a5c0bfc
sherlyfebrianti96 <sherlyfebrianti96@yahoo.co.id>
2021-04-10 21:39:16 +0200

    Create the DragAndDrop functionality for MoveLeftToRight and MoveRightToLeft

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import CoinArea from "./components/CoinArea/CoinArea";
 import {Col, Container, Row} from "react-bootstrap";
 import CoinConfiguration from "./components/CoinConfiguration/CoinConfiguration";
+import {DragDropContext, DropResult, ResponderProvided} from "react-beautiful-dnd";
 import {v4 as uuid} from "uuid";
 import {CoinValueInterface} from "./interface/CoinValueInterface";
 import {CoinValueEnum} from "./enum/CoinValueEnum";
@@ -38,6 +39,56 @@ export default class App extends React.Component {
     });
   }
 
+  handleDragEnd(result: DropResult, provided: ResponderProvided) {
+    const startingCoinDroppable = 'dropable-coin-area-starting-coin';
+    const neededCoinDroppable = 'dropable-coin-area-needed-coin';
+    const sourceId = result.source.droppableId;
+    const destinationId = result.destination?.droppableId;
+    const starterMoveToNeeded = ((sourceId === startingCoinDroppable) && (destinationId === neededCoinDroppable))
+    const neededMoveToStarter = ((sourceId === neededCoinDroppable) && (destinationId === startingCoinDroppable))
+
+    const sourceIndex: number = result.source.index;
+    const startingCoinArea = this.state.startingCoinArea;
+    const neededCoinArea = this.state.neededCoinArea;
+    let arrangementResult;
+    switch (true) {
+      case (starterMoveToNeeded) :
+        /* Left moved to right */
+        arrangementResult = this.arrangeItem(sourceIndex, startingCoinArea, neededCoinArea);
+        this.setState({
+          startingCoinArea: [...arrangementResult.source],
+          neededCoinArea: [...this.sortCoinArray(arrangementResult.destination)]
+        })
+        break;
+      case (neededMoveToStarter) :
+        /* Right moved to left */
+        arrangementResult = this.arrangeItem(sourceIndex, neededCoinArea, startingCoinArea);
+        this.setState({
+          startingCoinArea: [...arrangementResult.destination],
+          neededCoinArea: [...this.sortCoinArray(arrangementResult.source)]
+        })
+        break;
+      default:
+        /* Not moved anywhere */
+        break;
+    }
+  }
+
+  arrangeItem(sourceIndex: number, source:  Array<CoinValueInterface>, destination:  Array<CoinValueInterface>) {
+    const item = source[sourceIndex];
+    destination.push(item);
+    source.splice(sourceIndex, 1);
+
+    return {
+      source,
+      destination
+    };
+  }
+
+  sortCoinArray(arr: Array<CoinValueInterface>) {
+    return arr.sort((a, b) => (a.content - b.content));
+  }
+
   render() {
     return (
       <Container className="App">
@@ -47,23 +98,24 @@ export default class App extends React.Component {
           addCoinToStartedArea={this.addCoinToStartedArea.bind(this)}
           handleNeededCoinChanged={this.handleNeededCoinChanged.bind(this)}
         />
-        <Row>
-          <Col>
-            <CoinArea
-              name="starting-coin"
-              title="Starting Coin"
-              coins={this.state.startingCoinArea}
-            />
-          </Col>
-          <Col>
-            <CoinArea
-              name="needed-coin"
-              title="Needed Coin"
-              coins={this.state.neededCoinArea}
-              sort={true}
-            />
-          </Col>
-        </Row>
+        <DragDropContext onDragEnd={this.handleDragEnd.bind(this)}>
+          <Row>
+            <Col>
+              <CoinArea
+                name="starting-coin"
+                title="Starting Coin"
+                coins={this.state.startingCoinArea}
+              />
+            </Col>
+            <Col>
+              <CoinArea
+                name="needed-coin"
+                title="Needed Coin"
+                coins={this.state.neededCoinArea}
+              />
+            </Col>
+          </Row>
+        </DragDropContext>
       </Container>
     );
   }
--- src/components/CoinArea/CoinArea.tsx
+++ src/components/CoinArea/CoinArea.tsx
@@ -3,12 +3,19 @@ import './CoinArea.css';
 import {Card, Col, Row} from "react-bootstrap";
 import Coin from "../Coin/Coin";
 import {CoinValueInterface} from "../../interface/CoinValueInterface";
+import {
+  Draggable,
+  DraggableProvided,
+  DraggableStateSnapshot,
+  Droppable,
+  DroppableProvided,
+  DroppableStateSnapshot
+} from "react-beautiful-dnd";
 
 interface CoinAreaProps {
   name: string;
   title: string;
   coins: Array<CoinValueInterface>;
-  sort?: boolean;
 }
 
 class CoinArea extends React.PureComponent<CoinAreaProps> {
@@ -17,15 +24,25 @@ class CoinArea extends React.PureComponent<CoinAreaProps> {
     console.log(this.props.coins)
 
     const initialCoins = [...this.props.coins];
-    if (this.props.sort) {
-      initialCoins.sort((a, b) => (a.content - b.content));
-    }
 
-    const coins = initialCoins.map((coin: CoinValueInterface, index: number) => (
-      <Col key={`${areaId}-${index}`}>
-        <Coin value={coin.content} />
-      </Col>
-    ));
+    const coins = initialCoins.map((coin: CoinValueInterface, index: number) => {
+      const key = `${areaId}-${index}`;
+      return (
+        <Draggable key={key} draggableId={key} index={index}>
+          {(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => {
+            return (
+              <Col
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+              >
+                <Coin value={coin.content}/>
+              </Col>
+            );
+          }}
+        </Draggable>
+      )
+    });
 
     const boxValue = initialCoins.reduce((a: number, b: CoinValueInterface) => (a + b.content), 0);
 
@@ -34,11 +51,28 @@ class CoinArea extends React.PureComponent<CoinAreaProps> {
         <Card.Header>
           <h2>{this.props.title}</h2>
         </Card.Header>
-        <Card.Body className="Coin-Box">
-          <Row>
+        <Droppable droppableId={`dropable-${areaId}`}>
+          {((provided: DroppableProvided, snapshot: DroppableStateSnapshot) => {
+            return (
+              <Card.Body
+                className="Coin-Box"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                style={{
+                  background: snapshot.isDraggingOver ? 'lightBlue' : 'lightGrey'
+                }}
+              >
+                <Row>
+                  {coins}
+                </Row>
+                {provided.placeholder}
+              </Card.Body>
+            );
+          })}
+        </Droppable>
+        {/*<Row>
             {coins}
-          </Row>
-        </Card.Body>
+          </Row>*/}
         <Card.Footer className="CoinInformation">
           Current box value = {boxValue}
         </Card.Footer>
